c0504aca2ae18a473e0566b60883e61154e1f249
Amit2017 <mr.jingu007@gmail.com>
2020-03-03 10:47:11 +0530

    Small changes

--- app/src/main/java/com/example/sanskriti/odml/CheckStatus.java
+++ app/src/main/java/com/example/sanskriti/odml/CheckStatus.java
@@ -1,9 +1,13 @@
 package com.example.sanskriti.odml;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -15,18 +19,23 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CheckStatus extends AppCompatActivity {
 
+    private String TAG = "CheckStatus";
     private Intent intent;
     private String email;
     private String info;
     private String[] ODsplit;
     private ListView mylistview;
-
+    private HashMap<String,String[]> details;
+    private ArrayList<String> names;
     private String[] infoSplit;
+    private String res;
+    private int ODapproved = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,15 +43,146 @@ public class CheckStatus extends AppCompatActivity {
 
         intent = getIntent();
         email = intent.getStringExtra("email");
-
+        details = new HashMap<>();
         mylistview = findViewById(R.id.od_list);
 
         System.out.println("This is the email from intent - "+email);
 
         getDetails();
 
+        mylistview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int x = checkApproval();
+                if(x == -1)
+                {
+                    Log.d(TAG, "Approval status check failed.");
+                }
+                else
+                {
+                    ODapproved = Integer.parseInt(res);
+                }
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getApplicationContext());
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                if(ODapproved == 1)
+                {
+                    dialog.setMessage("OD Approved, close OD.");
+                    dialog.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            int x = closeOD();
+                            if(x==1)
+                            {
+                                Toast.makeText(CheckStatus.this, "OD closed successfully.", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                Toast.makeText(CheckStatus.this, "OD closed failed.", Toast.LENGTH_SHORT).show();
+                            }
+
+                            dialog.dismiss();
+                        }
+                    });
+                }
+
+            }
+        });
+
     }
 
+
+    private int checkApproval()
+    {
+        StringRequest request = new StringRequest(Request.Method.POST,Constants.CHECK_APPROVE_STATUS_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Toast.makeText(getApplicationContext(),response, Toast.LENGTH_LONG).show();
+                Log.d("CheckStatus", "Response is : " + response);
+                res = response;
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+                System.out.println("Error is " + error.toString());
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams()    throws AuthFailureError {
+                Map <String,String> params  = new HashMap<String,String>();
+
+                params.put("email",email);
+                //params.put("password",password);
+
+                return params;
+            }
+        };
+
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+
+        if(res.equals("ERROR OCCURED")){
+            return -1;
+        }
+        else
+        {
+            int temp = Integer.parseInt(res);
+            if(temp == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    private int closeOD()
+    {
+
+        StringRequest request = new StringRequest(Request.Method.POST,Constants.DELETE_OD_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Toast.makeText(getApplicationContext(),response, Toast.LENGTH_LONG).show();
+                Log.d("CheckStatus", "Response is : " + response);
+                res = response;
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+                System.out.println("Error is " + error.toString());
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams()    throws AuthFailureError {
+                Map <String,String> params  = new HashMap<String,String>();
+
+                params.put("email",email);
+                //params.put("password",password);
+
+                return params;
+            }
+        };
+
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+
+        if(res.equals("Record updated successfully")){
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
     private void getDetails(){
         StringRequest request = new StringRequest(Request.Method.POST,Constants.APPROVE_CHECK_URL, new Response.Listener<String>() {
             @Override
@@ -52,13 +192,14 @@ public class CheckStatus extends AppCompatActivity {
 
                 info = response;
                 ODsplit = info.split("\n");
-
+                names = new ArrayList<>();
                 for(int i=0; i<ODsplit.length; i++)
                 {
-                    infoSplit = ODsplit[i].split(",");
+                    String[] temp = ODsplit[i].split(",");
+                    names.add("From :"+temp[2]+" , To: "+temp[3]);
                 }
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
-                        android.R.layout.simple_list_item_1, infoSplit);
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
+                        android.R.layout.simple_list_item_1, names);
 
                 mylistview.setAdapter(adapter);
             }
@@ -71,7 +212,7 @@ public class CheckStatus extends AppCompatActivity {
         })
         {
             @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
+            protected Map<String, String> getParams()    throws AuthFailureError {
                 Map <String,String> params  = new HashMap<String,String>();
 
                 params.put("email",email);
--- app/src/main/java/com/example/sanskriti/odml/Constants.java
+++ app/src/main/java/com/example/sanskriti/odml/Constants.java
@@ -7,4 +7,6 @@ public class Constants {
     public static final String WRITE_DETAILS_URL= DB_URL+"insert_od_details.php";
     public static final String APPROVE_CHECK_URL= DB_URL+"approve_check.php";
     public static final String UPDATE_OD_URL= DB_URL+"update_od.php";
+    public static final String DELETE_OD_URL= DB_URL+"delete_od.php";
+    public static final String CHECK_APPROVE_STATUS_URL= DB_URL+"student_check_approval.php";
 }
