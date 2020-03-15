5e100da305618da1c8a3d3ea199aacf5756534b2
Sanskriti-Midha <sanskritimidha@gmail.com>
2020-03-15 13:19:23 +0530

    added LoginTest2

---
+++ app/src/androidTest/java/com/example/sanskriti/odml/Authentication/LoginTest2.java
@@ -0,0 +1,153 @@
+package com.example.sanskriti.odml.Authentication;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.example.sanskriti.odml.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginTest2 {
+
+    @Rule
+    public ActivityTestRule<Login> mActivityTestRule = new ActivityTestRule<>(Login.class);
+
+    @Test
+    public void loginTest2() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.EmailEditText),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("CB.EN.U4CSE17201"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.PasswordEditText),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("cse17201"), closeSoftKeyboard());
+
+        ViewInteraction appCompatCheckBox = onView(
+                allOf(withText("Remember Me"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatCheckBox.perform(click());
+
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.EmailEditText), withText("CB.EN.U4CSE17201"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        editText.check(matches(withText("CB.EN.U4CSE17201")));
+
+        ViewInteraction editText2 = onView(
+                allOf(withId(R.id.PasswordEditText), withText("••••••••"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                2),
+                        isDisplayed()));
+        editText2.check(matches(withText("••••••••")));
+
+        ViewInteraction checkBox = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                0),
+                        3),
+                        isDisplayed()));
+        checkBox.check(matches(isDisplayed()));
+
+        ViewInteraction textView = onView(
+                allOf(withText("Forgot Password"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                4),
+                        isDisplayed()));
+        textView.check(doesNotExist());
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.login_button),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                5),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.login_button), withText("Log In"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                5),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
